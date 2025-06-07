90008b4d6b65f48f5556a794f02494f6ff64fc19
Saint khen <72024308+emmogrin@users.noreply.github.com>
2025-06-07 05:08:50 +0100

    install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -2,32 +2,15 @@
 set -e
 
 echo "Updating system packages..."
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
 echo "Installing basic utilities and dependencies..."
-sudo apt install -y curl ufw iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev
+apt install -y curl ufw iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev
 
-echo "Installing Docker's official GPG key and repo..."
-sudo apt-get install -y ca-certificates curl gnupg
-
-sudo install -m 0755 -d /etc/apt/keyrings
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-echo "Updating package lists again..."
-sudo apt update -y && sudo apt upgrade -y
-
-echo "Installing Docker packages..."
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Installing Docker via official Docker install script..."
+curl -fsSL https://get.docker.com | sh
 
 echo "Testing Docker installation..."
-sudo docker run hello-world
+docker run hello-world
 
 echo "Docker installation completed successfully!"
